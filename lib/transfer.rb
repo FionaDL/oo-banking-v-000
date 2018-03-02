90e586bb58c598de199d8cae7a96400aebe475fa
Fiona Lapham <fionadlapham@gmail.com>
2018-03-02 16:48:17 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
     end
 
   def reverse_transfer
-    if  valid? && receiver.balance > amount && self.status == "complete"
+    if self.status == "complete"
       sender.balance += amount
       receiver.balance -= amount
       self.status = "reversed"
